441e2d962d5e16df05e292190afe50b733b8ebac
DuskoPeric <Dusko.Peric@homegate.ch>
2021-04-22 18:04:21 +0200

    Let's start

---
+++ src/components/shared/header.js
@@ -0,0 +1,8 @@
+import './header.scss';
+const Header = () => {
+    return <header className="App-header">
+        <h1 className="App-title">DP-Games</h1>
+    </header>
+}
+
+export default Header;
